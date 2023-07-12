a12036c732a74c3a2cb23b1113a1eafa1ffb1307
Bernardo Santiago Marin <bernardosantiago44@gmail.com>
2023-07-11 19:37:31 -0600

    Initial Commit

---
+++ Schedules/SchedulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulesApp.swift
+//  Schedules
+//
+//  Created by Bernardo Santiago Marin on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SchedulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
